c903107ce11e7290e51c0e1ca416258d6410640b
Navi Kaur <navdeep9520@gmail.com>
2021-09-04 20:31:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const main = document.querySelector('#main')
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.innerHTML = "Blake is the champion"
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
